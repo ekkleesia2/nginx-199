e67a83ff95b62a436234a5df22703d64a59aa101
ekkleesia2 <ekkleesia2@users.noreply.github.com>
2015-09-26 01:17:07 -0430

    Update run_2.sh

--- run_2.sh
+++ run_2.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cat /app/nginx/conf/nginx.conf | grep listen
 sed -ig 's,listen       80,listen       '$PORT',' /app/nginx/conf/nginx.conf
 echo hello
+cat /app/nginx/conf/nginx.conf | grep listen
 /app/nginx/sbin/nginx
